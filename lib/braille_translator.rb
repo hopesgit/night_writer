768141d80e675d9728e0bc05cf1a2902d711b855
Hope Gochnour <hope.gochnour@gmail.com>
2020-07-31 22:20:10 -0600

    Add new test to BrailleTranslator/Test; attibutes are readable

--- lib/braille_translator.rb
+++ lib/braille_translator.rb
@@ -1,4 +1,5 @@
 class BrailleTranslator
+  attr_reader :text_to_translate, :direction
 
   def initialize(text_to_translate, direction="Braille")
     @text_to_translate = text_to_translate
--- test/braille_translator_test.rb
+++ test/braille_translator_test.rb
@@ -8,4 +8,11 @@ class BrailleTranslatorTest < Minitest::Test
     trans = BrailleTranslator.new("empty")
     assert_instance_of BrailleTranslator, trans
   end
+
+  def test_it_has_details
+    trans = BrailleTranslator.new("empty")
+    assert_equal "empty", trans.text_to_translate
+    assert_equal "Braille", trans.direction
+  end
+
 end

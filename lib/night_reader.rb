993b2bf9e32f45e5862340f6ec2e7f91c0f7b363
Hope Gochnour <hope.gochnour@gmail.com>
2020-08-04 08:15:22 -0600

    Update NightReader, not complete

--- lib/night_reader.rb
+++ lib/night_reader.rb
@@ -6,4 +6,65 @@ class NightReader
   def initialize
     @reader = FileReader.new
   end
+
+  def read
+    @reader.read(ARGV[0])
+    @reader.create
+    @reader.file_text
+  end
+
+  # def encode_file_to_braille
+  #   start = read
+  #   braille = encode_to_braille(plain)
+  #   text_to_add = too_many_characters(braille)
+  #   File.write(ARGV[1], text_to_add)
+  #   p "#{ARGV[1]} created with #{text_to_add.length} characters!"
+  # end
+  #
+  def encode_to_english(input)
+    require "pry"; binding.pry
+    input_array_ver = input.chars
+    input_array_ver.map do |input_item|
+      @reader.all_characters[input_item]
+    end
+  end
+  #
+  # def too_many_characters(braille)
+  #   final_text = ""
+  #   if braille.length > 40
+  #     divisor = braille.length / 40
+  #     segments = []
+  #     if braille.length % 40.0 == 0
+  #       divisor.times do
+  #         segments << braille.slice!(0..39)
+  #       end
+  #       segments.each do |block|
+  #         final_text += translate_to_braille(block)
+  #       end
+  #     else
+  #       divisor.times do
+  #         segments << braille.slice!(0..39)
+  #       end
+  #       segments << braille.slice!(0..-1)
+  #       segments.each do |block|
+  #         final_text += translate_to_braille(block)
+  #       end
+  #     end
+  #   else
+  #     final_text += translate_to_braille(braille)
+  #   end
+  #   final_text
+  # end
+  #
+  # def translate_to_braille(text_to_translate)
+  #   first_part = ""
+  #   second_part = ""
+  #   third_part = ""
+  #   text_to_translate.each do |text|
+  #     first_part += text[0..1]
+  #     second_part += text[2..3]
+  #     third_part += text[4..5]
+  #   end
+  #   first_part + "\n" + second_part + "\n" + third_part + "\n"
+  # end
 end
--- test/night_reader_test.rb
+++ test/night_reader_test.rb
@@ -14,4 +14,24 @@ class NightReaderTest < Minitest::Test
 
     assert_instance_of FileReader, understanding.reader
   end
+
+  def test_it_has_read_the_suggested_file
+    understanding = NightReader.new
+    understanding.read
+
+    assert_equal ".00..0.0...0.0..0....00..0.0..0.00...00.0...0.000.0.000.000000..0.0.0.0.00000..0\n00000.0...0.0.......00.00.00...00...0000.0...0...00000.0.0...0..0.00.0...0....0.\n0.....0.....0.......0...0.0...0.....0.........0...0.....0...00..000.0.........0.\n.0...000.0.00.00..000.0.0..00...000...000..0..0.0...0.0.0.0.000.00..\n00..0..00.00.0....0.0..0..0..0...0.0...0.000..0..0....0...00...0.0..\n0...0.000.0...0...0.0.....0.......0...0.0.0...00......0...0.0.......", understanding.reader.file_text
+  end
+
+  def test_it_can_encode_to_english
+  end
+
+  def test_it_can_encode_files_to_english
+  end
+
+  def translate_to_english
+    understanding = NightReader.new
+    start = understanding.read
+
+    assert_equal "", understanding.encode_to_english(start)
+  end
 end

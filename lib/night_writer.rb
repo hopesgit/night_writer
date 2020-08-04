44ef837dd7bd8ce1b7079873e20ba49f58632c85
Hope Gochnour <hope.gochnour@gmail.com>
2020-08-04 03:49:23 -0600

    Update: Writer can write Braille to a file

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,12 +1,10 @@
 require "./lib/file_reader"
-require "./lib/braille_translator"
 
 class NightWriter
-  attr_reader :reader, :translator
+  attr_reader :reader
 
   def initialize
     @reader = FileReader.new
-    @translator = BrailleTranslator.new("")
   end
 
   def read
@@ -18,6 +16,9 @@ class NightWriter
   def encode_file_to_braille
     plain = read
     braille = encode_to_braille(plain)
+    text_to_add = translate_to_braille(braille)
+    File.write("./braille.txt", text_to_add)
+    p "Check the file!"
   end
 
   def encode_to_braille(input)
@@ -25,6 +26,19 @@ class NightWriter
     input_array_ver.map do |input_item|
       @reader.all_characters[input_item]
     end
+  end
+
+  def translate_to_braille(text_to_translate)
+    # newline_placement = text_to_translate.count * 2
+    first_part = ""
+    second_part = ""
+    third_part = ""
+    text_to_translate.each do |text|
+      first_part += text[0..1]
+      second_part += text[2..3]
+      third_part += text[4..5]
+    end
+    first_part + "\n" + second_part + "\n" + third_part
   end
 end
 
--- test/night_writer_test.rb
+++ test/night_writer_test.rb
@@ -1,6 +1,7 @@
 require "./lib/night_writer"
 require "minitest/autorun"
 require "minitest/pride"
+require "mocha/minitest"
 
 class NightWriterTest < Minitest::Test
 
@@ -10,18 +11,32 @@ class NightWriterTest < Minitest::Test
     assert_instance_of NightWriter, writer
   end
 
-  def test_it_has_attributes
+  def test_it_has_a_reader
     writer = NightWriter.new
 
     assert_instance_of FileReader, writer.reader
-    assert_instance_of BrailleTranslator, writer.translator
   end
 
   def test_encoding_to_braille
     writer = NightWriter.new
+    writer.reader.stubs(:file_text).returns("hello world! ")
     plain = writer.read
     expected = ["0.00..", "0..0..", "0.0.0.", "0.0.0.", "0..00.", "......", ".000.0", "0..00.", "0.000.", "0.0.0.", "00.0..", "..000.", "......"]
 
     assert_equal expected, writer.encode_to_braille(plain)
+  end
+
+  def test_translating_to_braille
+    writer = NightWriter.new
+    plain = writer.read
+    braille = writer.encode_to_braille(plain)
+
+    assert_equal "0.0.0.0.0....00.0.0.00....\n00.00.0..0..00.0000..000..\n....0.0.0....00.0.0...0...", writer.translate_to_braille(braille)
+  end
+
+  def test_encoding_files_to_braille
+    writer = NightWriter.new
+
+    assert_equal "Check the file!", writer.encode_file_to_braille
   end
 end
